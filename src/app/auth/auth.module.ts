d5c940e50f20013b282feaef001ff716368a3118
srAndrii <55akif5548@gmail.com>
2022-09-23 12:34:59 +0300

    Login.Added login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -12,15 +12,20 @@ import { RegisterEffect } from './store/effects/register.effect'
 import { BackendErrorMessagesModule } from '../shared/modules/backendErrorMessages/backendErrorMessages.module'
 import { PersistenceService } from '../shared/services/persistence.service'
 import { LoginEffect } from './store/effects/login.effect'
+import { LoginComponent } from './components/login/login.component'
 
 const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent,
     },
+    {
+        path: 'login',
+        component: LoginComponent,
+    },
 ]
 @NgModule({
-    declarations: [RegisterComponent],
+    declarations: [RegisterComponent, LoginComponent],
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { select, Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
+
+import {
+    isSubmittingSelector,
+    validationErrorsSelector,
+} from '../../store/selectors'
+import { AuthService } from '../../services/auth.service'
+import { BackendErrorsInterface } from '../../../shared/types/backendErrors.interface'
+import { LoginRequestInterface } from '../../types/loginRequest.interface'
+import { loginAction } from '../../store/actions/login.action'
+
+@Component({
+    selector: 'mc-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+    form: FormGroup
+    isSubmitting$: Observable<boolean>
+    backendErrors$: Observable<BackendErrorsInterface | any>
+
+    constructor(
+        private fb: FormBuilder,
+        private store: Store,
+        private authService: AuthService
+    ) {}
+
+    ngOnInit(): void {
+        this.initializeForm()
+        this.initializeValue()
+    }
+
+    initializeForm(): void {
+        this.form = this.fb.group({
+            email: '',
+            password: '',
+        })
+        console.log(this.form.valid)
+    }
+
+    initializeValue(): void {
+        this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+        this.backendErrors$ = this.store.pipe(select(validationErrorsSelector))
+    }
+
+    onSubmit(): void {
+        const request: LoginRequestInterface = {
+            user: this.form.value,
+        }
+        this.store.dispatch(loginAction({ request }))
+    }
+}
