985c79cd96291b3d85fff4c0caf638325cf7db65
srAndrii <55akif5548@gmail.com>
2022-10-04 19:12:51 +0300

    Feed module.Created Feed module and Global Feed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EffectsModule } from '@ngrx/effects'
 import { TopBarModule } from './shared/modules/topBar/topBar.module'
 import { PersistenceService } from './shared/services/persistence.service'
 import { AuthInterceptor } from './shared/services/authInterceptor'
+import { GlobalFeedModule } from './globalFeed/globalFeed.module'
 
 @NgModule({
     declarations: [AppComponent],
@@ -27,6 +28,7 @@ import { AuthInterceptor } from './shared/services/authInterceptor'
         }),
         EffectsModule.forRoot([]),
         TopBarModule,
+        GlobalFeedModule,
     ],
     providers: [
         PersistenceService,
---
+++ src/app/shared/modules/feed/feed.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FeedComponent } from './components/feed/feed.component'
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [FeedComponent],
+    exports: [FeedComponent],
+})
+export class FeedModule {}
