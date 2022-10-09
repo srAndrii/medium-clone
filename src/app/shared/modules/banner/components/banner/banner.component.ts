da8dae3bb5a21385daee05dfc0c86e1a22f655de
srAndrii <55akif5548@gmail.com>
2022-10-09 12:39:37 +0300

    Feed module.Added Error component, Loading component and Banner component

---
+++ src/app/shared/modules/banner/components/banner/banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'mc-banner',
+    styleUrls: ['banner.component.scss'],
+    templateUrl: 'banner.component.html',
+})
+export class BannerComponent {}
